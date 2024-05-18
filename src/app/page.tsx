f99f5b45734075d41b33e1ab06f0e983807cbffd
Piyush Paul <piyushpaul605@gmail.com>
2024-05-18 23:57:20 +0530

    finished Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
       <Statistics/>
       <CTA/>
       <Footer/>
-      Hello World
     </main>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,24 @@
-import { Inter } from "next/font/google"
-import Link from "next/link"
+import { Inter } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
 
 
 export const inter = Inter({
     subsets: ['latin'],
     weight: ["100", "200", "300", "400", "500", "600", "700"]
-  })
+  });
 
 export default function Footer() {
     return (
         <div className={`${inter.className} bg-black  text-white`}>
             <div className="flex md:flex-row flex-col justify-between items-center mx-20 pt-10 pb-10">
                 <div className="flex flex-wrap">
-                    <h1 className="text-2xl">PIYUSH PAUL</h1>
+                    <h1 className="text-2xl md:pb-0 pb-5">OM</h1>
                 </div>
                 <div className="flex flex-row gap-x-10">
-                    <Link href='/' className="cursor-none">Home</Link>
-                    <Link href='/' className="cursor-none">About</Link>
-                    <Link href='/' className="cursor-none">Contact me</Link>
+                    <Link href='/' className="cursor-none transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110">Home</Link>
+                    <Link href='/' className="cursor-none transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110">About</Link>
+                    <Link href='/' className="cursor-none transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110">Contact me</Link>
                 </div>
             </div>
             <div className="flex md:flex-row flex-col justify-between items-center mx-20 pb-10">
@@ -25,9 +26,9 @@ export default function Footer() {
                     <h1 className="text-md font-light">Developed by Piyush Paul</h1>
                 </div>
                 <div className="flex flex-row gap-x-10">
-                    <Link href='/' className="cursor-none relative inline-flex items-center justify-center w-10 h-10 rounded-full bg-black text-white hover:text-black transition-colors">Home</Link>
-                    <Link href='/' className="cursor-none relative inline-flex items-center justify-center w-10 h-10 rounded-full bg-black text-white hover:text-black transition-colors">About</Link>
-                    <Link href='/' className="cursor-none relative inline-flex items-center justify-center w-10 h-10 rounded-full bg-black text-white hover:text-black transition-colors">Contact me</Link>
+                    <Link href='https://x.com/piyushpaul_'><Image className="rounded-full cursor-none transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110" src={'/img/twitter.svg'} alt="Twitter" width={25} height={25}></Image></Link>
+                    <Link href='https://github.com/Piyush800x'><Image className="rounded-full cursor-none transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110" src={'/img/github.svg'} alt="GitHub" width={25} height={25}></Image></Link>
+                    <Link href='https://linkedin.com/in/piyushpaul'><Image className="rounded-full cursor-none transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110" src={'/img/ll.svg'} alt="LinkedIn" width={25} height={25}></Image></Link>
                 </div>
             </div>
         </div>
