156e1dd3fa4d432508e4c5d6572b2eec5a881bda
Piyush Paul <piyushpaul605@gmail.com>
2024-05-30 21:22:45 +0530

    responsive HirePage

---
+++ src/app/api/submit/route.ts
@@ -0,0 +1,6 @@
+import { headers } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    // use nodemailer to send email
+}
