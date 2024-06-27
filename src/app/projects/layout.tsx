3cb3096cb667822f70cfaf3d891df55f4523bf41
Piyush Paul <piyushpaul605@gmail.com>
2024-06-28 01:37:23 +0530

    small changes

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html>
+        <html lang="en">
             <body>
                 {children}
             </body>
